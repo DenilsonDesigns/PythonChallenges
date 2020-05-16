59d70bd4b7ab5d64964f1be08afbcac7137b1c9e
Daniel <daniellanguilller@gmail.com>
2020-05-16 13:33:14 +1000

    wip

--- very_easy.py
+++ very_easy.py
@@ -136,4 +136,216 @@ def hello_world(num):
     if not num % 5: return 'World'
 
 
-print(hello_world(3))
+def greater_than_one(frac):
+    return (int(frac.split("/")[0]) / int(frac.split("/")[1])) > 1
+
+
+# faster python way
+# def greaterThanOne(frac):
+#     return eval(frac) > 1
+
+def googlify(n):
+    if n < 2:
+        return "invalid"
+    return "G" + n * "o" + "gle"
+
+
+# def googlify(n):
+# 	return 'G{}gle'.format('o'*n) if n > 1 else 'invalid'
+
+def is_triangle(a, b, c):
+    if (a + b > c) and (a + c > b) and (b + c > a):
+        return True
+    return False
+
+
+# def is_triangle(*sides):
+#     return all(s < sum(sides) - s for s in sides)
+
+
+def pH_name(pH):
+    if pH < 0 or pH > 14:
+        return "invalid"
+    return "acidic" if 7 > pH else "alkaline" if pH > 7 else "neutral"
+
+
+def detect_word(txt):
+    return "".join([let for let in txt if let.lower() == let])
+
+
+# def detect_word(txt):
+# 	return "".join(c for c in txt if c.islower())
+
+def skip_the_sugar(drinks):
+    return [drink for drink in drinks if drink not in ['fanta', 'cola']]
+
+
+def has_hidden_fee(prices, t):
+    return (sum(int(price.strip("$")) for price in prices)) < int(t.strip("$"))
+
+
+def my_func(string):
+    return [s for s in string if s.isnumeric()], [s for s in string if s.isalpha()]
+
+
+def add_odd_to_n(n):
+    return sum(num for num in range(n + 1) if num % 2 != 0)
+
+
+def Long_Burp(num):
+    return "Bu" + num * "r" + "p"
+
+
+def eq(evaluate):
+    return evaluate
+
+
+def convert(letter):
+    return ord(letter)
+
+
+def has_same_bread(lst1, lst2):
+    # return lst1[::len(lst1) - 1]
+    return lst1[::len(lst1) - 1] == lst2[::len(lst2) - 1]
+
+
+def is_it_true(relation):
+    if "=" in relation:
+        return relation.split("=")[0] == relation.split("=")[1]
+    return eval(relation)
+
+
+def halfQuarterEighth(n):
+    return [n / 2, n / 4, n / 8]
+
+
+def potatoes(potato):
+    return potato.count("potato")
+
+
+def backwords(str):
+    return str[::-1]
+
+
+def sum_first_n_nums(lst, n):
+    return sum(lst[0:n])
+
+
+def simplePigLatin(word):
+    return word[1:] + word[0] + "ay"
+
+
+def get_triangle_type(lst):
+    if len(lst) > 3 or len(lst) < 3:
+        return "not a triangle"
+    if len(set(lst)) == 1:
+        return "equilateral"
+    if len(set(lst)) == 2:
+        return "isosceles"
+    return 'scalene'
+
+
+def convert(data1, data2):
+    return type(data1)(data2)
+
+
+def reverse_title(txt):
+    return txt.title().swapcase()
+
+
+d1 = {'D': 1, 'B': 2, 'C': 3}
+d2 = {'likes': 2, 'dislikes': 3, 'followers': 10}
+
+
+def dict_to_list(d):
+    return sorted(d.items())
+
+
+def squares_sum(n):
+    return sum(i ** 2 for i in range(0, n + 1))
+
+
+def find_the_falsehoods(lst):
+    return [i for i in lst if not i]
+
+
+def is_repeated(strn):
+    return strn
+
+
+def reverse_list(num):
+    return [int(i) for i in list(str(num))[::-1]]
+
+
+def zip_it(women, men):
+    return list(zip(women, men)) if len(women) == len(men) else "Sizes don't match"
+
+
+def max_total(nums):
+    return sum(sorted(nums)[-5:])
+
+
+def is_potential_friend(set1, set2):
+    return True if len(set1 & set2) >= 2 or set1 == set2 else False
+    # return set1 & set2
+
+
+def number_args(*argv):
+    return len(argv)
+
+
+def sub_reddit(link):
+    return link.split('/')[-2]
+
+
+def filter_unique(lst):
+    return [item for item in lst if len(item) == len(set(item))]
+
+
+def strip_sentence(txt, chars):
+    return ''.join([char for char in txt if char not in chars])
+
+
+def add_nums(nums):
+    return sum(int(num) for num in nums.split(', '))
+
+
+def count_ones(matrix):
+    return sum([row.count(1) for row in matrix])
+
+
+def length(txt):
+    if len(txt) == 0:
+        return 0
+    return length(txt[0:-1]) + 1
+
+
+def findHighest(list, higher=0):
+    if len(list) == 0:
+        return higher
+    highest = higher
+    if list[0] > highest:
+        highest = list[0]
+    return findHighest(list[1:], highest)
+
+
+def one_odd_one_even(n):
+    arr = [int(i) for i in str(n)]
+    if arr[0] % 2 == 0 and arr[1] % 2 != 0:
+        return True
+    if arr[0] % 2 != 0 and arr[1] % 2 == 0:
+        return True
+    return False
+
+
+def add_up(n):
+    if n == 1:
+        return n
+    else:
+        return n + add_up(n - 1)
+
+
+
+
+print(add_up(4))
+print(add_up(1000))
